305cef8f4a75d2afe06fdbae0060c7a61bf7e77c
Brandy <zhangzihao@zhangzihaodemac-mini.local>
2018-08-01 11:24:43 +0800

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '11.0'
+# Uncomment this line if you're using Swift
+#source 'https://github.com/CocoaPods/Specs.git'
+#pod 'Spring', :git => 'https://github.com/MengTo/Spring.git', :branch => 'swift3'
+
+use_frameworks!
+inhibit_all_warnings!
+target 'testPandoraPlayer' do
+
+#pod 'PandoraPlayer'
+pod 'AudioKit', '~> 4.0'
+
+pod "FileKit"
+end
+
---
+++ testPandoraPlayer/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  testPandoraPlayer
+//
+//  Created by 张子豪 on 2018/8/1.
+//  Copyright © 2018年 zhangqian. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+import MediaPlayer
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func 测试BTN(_ sender: Any) {
+        
+        
+//        var AVPlayerItemX = MPMediaItem(coder: <#T##NSCoder#>)
+//        
+//        let playerVC = PandoraPlayer.configure(withMPMediaItems: [AVPlayerItemX])
+//        navigationController?.present(playerVC, animated: true, completion: nil)
+        
+    }
+}
+
